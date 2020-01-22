2658f40e1bae3f589821d9b0b32871f8f44a81d1
naohiro nakagawa <mylabosango@gmail.com>
2020-01-22 13:36:21 +0900

    add launch file

---
+++ build/CMakeFiles/leptrino_force_torque.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leptrino_force_torque.dir/src/leptrino_force_torque.cpp.o"
+  "CMakeFiles/leptrino_force_torque.dir/src/rs_comm.cpp.o"
+  "devel/lib/leptrino_force_torque/leptrino_force_torque.pdb"
+  "devel/lib/leptrino_force_torque/leptrino_force_torque"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leptrino_force_torque.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lleptrino_force_torque".split(';') if "-lleptrino_force_torque" != "" else []
+PROJECT_NAME = "leptrino_force_torque"
+PROJECT_SPACE_DIR = "/usr/local"
+PROJECT_VERSION = "1.0.0"
